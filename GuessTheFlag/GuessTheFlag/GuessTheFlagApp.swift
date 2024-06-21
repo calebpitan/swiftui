6baa1b7e89cfa5dbc4263506de9f3eabb1f925ca
Caleb Adepitan <calebpitan@gmail.com>
2024-06-21 08:57:01 +0100

    move files around and add a new app

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Caleb Adepitan on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
