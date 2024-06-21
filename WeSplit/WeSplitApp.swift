609532715a1a3526cd0ade25fc65a9ac0258ad2e
Caleb Adepitan <calebpitan@gmail.com>
2024-06-21 08:29:42 +0100

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Caleb Adepitan on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
